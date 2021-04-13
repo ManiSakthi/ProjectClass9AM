993d95b772f93fe64c511d4bd3745081846bb18f
ManiSakthi <manisakthivpp@gmail.com>
2021-04-13 22:02:57 +0530

    my task done

---
+++ test/java/org/sample/BaseClass.java
@@ -0,0 +1,14 @@
+package org.sample;
+
+import java.sql.Driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseClass {
+public static void setpath() {
+	System.setProperty(WebDriver.chrome.driver, arg1);
+WebDriver driver = new ChromeDriver();
+
+}
+}
